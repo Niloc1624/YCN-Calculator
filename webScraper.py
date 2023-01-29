de611e14f96cc795c0acc2acd33eb41fce0faaba
Colin Richter <114442447+Niloc1624@users.noreply.github.com>
2023-01-29 01:17:38 -0600

    added lots of comments

--- webScraper.py
+++ webScraper.py
@@ -14,13 +14,15 @@ last_name = "last_name"
 
 
 def webScraper(first_name, last_name):
-
+    '''
+    Scrapes https://results.o2cm.com/ given someone's name.
+    '''
     # config
     show_browser = 0
-    show_work = 1
+    show_work = 0
     debug_reject_headers = 1
 
-    ##Make blank tables
+    ##Make blank tables - there's probably a way to automate this but...
     smooth_data = {
         "newcomer": {
             "waltz": [0, 0],
@@ -219,8 +221,7 @@ def webScraper(first_name, last_name):
         },
     }
 
-    ##Open Chrome and search for a dancer
-
+    # Open Chrome and search for a dancer
     op = webdriver.ChromeOptions()
     if not show_browser:
         op.add_argument("headless")  # makes it so Chrome doesn't actually open
@@ -238,59 +239,51 @@ def webScraper(first_name, last_name):
     last_name_field.send_keys(last_name)
     search.click()
 
-    ## Do shit with results
-
+    # Find all the results on the page
     all_results = driver.find_elements(By.PARTIAL_LINK_TEXT, ") ")
 
     # Get results in top 6
     top6_results_links = []
     for i, result in enumerate(all_results):
-        # if result.get_attribute("innerHTML")[0:2] == "3)":
-        #    print("starts with 3)")
-
-        # print(result.get_attribute("innerHTML"))
         if resultValid(driver, result):
             top6_results_links.append(Result(result, driver, debug_reject_headers))
 
     # Check rounds
     for result in top6_results_links:
-
+        # Calculate dances - have to do this now because now we have all the links
         result.calculateDances()
 
         # Open results page
         driver.execute_script("window.open('');")
         driver.switch_to.window(driver.window_handles[1])
-        # print(result_link)
         driver.get(result.link)
 
         # Count number of rounds in event
         options = driver.find_elements(By.CSS_SELECTOR, "option")
         num_rounds = len(options)
 
-        # If event is straight semi, did we get top 3?
+        # Check if we earned points. If we did, add those points to the data
         if (num_rounds == 2 and result.placement <= 3) or (
             num_rounds > 2 and result.placement <= 6
         ):
             if result.placement <= 3:
                 for dance in result.dances:
-                    # print(result.style + "_data")
                     num_points = max(4 - result.placement, 1)
                     for i in range(2):
                         eval(result.style + "_data")[result.level][dance][
                             i
                         ] += num_points
-
-                # print(result.raw_text)
+                # Mainly for troubleshooting, will print to terminal every round we got points
                 if show_work:
                     print(
                         f"{result} There were {num_rounds} rounds. Adding {num_points} point(s)."
                     )
-                # print()
 
         # Close results page
         driver.close()
         driver.switch_to.window(driver.window_handles[-1])
 
+    # Calculate the double points for level below rule
     for data in [smooth_data, standard_data, rhythm_data, latin_data]:
         levels = list(data)[::-1]
         for i, level in enumerate(levels):
@@ -301,6 +294,8 @@ def webScraper(first_name, last_name):
                     new_point_total = 2 * data[higher_level][dance][0] + current_points
                     data[level][dance][0] = new_point_total
 
+    # Probably there's a way to automate this
+    # Make dictionaries to dataframes
     smooth_df = pd.DataFrame(smooth_data)
     standard_df = pd.DataFrame(standard_data)
     rhythm_df = pd.DataFrame(rhythm_data)
@@ -308,6 +303,7 @@ def webScraper(first_name, last_name):
 
     data_frames = [smooth_df, standard_df, rhythm_df, latin_df]
 
+    # Column labels
     for df in data_frames:
         df.columns = [
             "Newcomer",
@@ -319,20 +315,29 @@ def webScraper(first_name, last_name):
             "Champ",
         ]
 
+    # Row labels
     smooth_df.index = ["Waltz", "Tango", "Foxtrot", "V. Waltz"]
     standard_df.index = ["Waltz", "Tango", "V. Waltz", "Foxtrot", "Quickstep"]
     rhythm_df.index = ["Cha cha", "Rumba", "Swing", "Bolero", "Mambo"]
     latin_df.index = ["Samba", "Cha cha", "Rumba", "Paso Doble", "Jive"]
 
-    output = [f"Results for {first_name} {last_name}"]
+    # Lots of words to go at the top of the webpage
+    output = [f"Results for {first_name} {last_name} from https://results.o2cm.com/."]
+    output.append(
+        "YCN points are calculated according to this system: http://ballroom.mit.edu/index.php/ycn-proficiency-points/."
+    )
 
     output.append(
         'The first number is the number of the points including the "double points each level down" rule. This is the number that matters'
     )
     output.append(
+        "For example, 2 gold points is worth 4 silver points. The first number includes this in its calculation. The second number does not."
+    )
+    output.append(
         "The second number is the number of the points earned by reaching finals in that level. This number is just fyi."
     )
 
+    # The goods (data)
     output.append("Smooth")
     output.append(smooth_df)
 
@@ -348,8 +353,9 @@ def webScraper(first_name, last_name):
     return output
 
 
-'''output = webScraper(first_name, last_name)
+# For testing
+"""output = webScraper(first_name, last_name)
 
 for line in output:
     print()
-    print(line)'''
+    print(line)"""
