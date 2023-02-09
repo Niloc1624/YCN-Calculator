f06c3ca5532cdd0b534bc85de66862533a6e6596
Colin Richter <114442447+Niloc1624@users.noreply.github.com>
2023-02-09 00:44:08 -0600

    Moved functionality to utils

--- utils.py
+++ utils.py
@@ -10,3 +10,18 @@ def lists_both_have_ele_in_str(list1, list2, string):
     Returns if string is in both list1 and list2.
     """
     return which_ele_is_in_str(list1, string) and which_ele_is_in_str(list2, string)
+
+
+def remove_TBAs_and_dups(competitor_name_elements_with_TBAs_and_dups):
+    """
+    Removes entries that start with TBA (case-sensitive) and duplicate names.
+
+    competitor_name_elements_with_TBAs_and_dups : list of beautiful soup elements
+    """
+    competitor_names = []
+    competitor_name_elements = []
+    for ele in competitor_name_elements_with_TBAs_and_dups:
+        if ele.text[:3] != "TBA" and ele.text not in competitor_names:
+            competitor_name_elements.append(ele)
+            competitor_names.append(ele.text)
+    return competitor_name_elements
